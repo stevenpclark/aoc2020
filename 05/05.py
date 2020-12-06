dc3af2f0dc9c16cc8d00310e3f882ae776926b8c
Steven Clark <steven@macbook-pro.lan>
2020-12-06 10:25:26 -0500

    refactor 5 to be a bit cleaner

--- 05/05.py
+++ 05/05.py
@@ -1,23 +1,12 @@
-REMAP_ROW = {'F':'0', 'B':'1'}
-REMAP_COL = {'L':'0', 'R':'1'}
-
-def get_row_col(s):
-    s_r = s[:7]
-    s_c = s[7:]
-    for k,v in REMAP_ROW.items():
-        s_r = s_r.replace(k, v)
-    for k,v in REMAP_COL.items():
-        s_c = s_c.replace(k, v)
-    return int(s_r, 2), int(s_c, 2)
+REMAP = {'F':'0', 'B':'1', 'L':'0', 'R':'1'}
 
 def get_seat_id(s):
-    r, c = get_row_col(s)
-    return r*8 + c
-
+    for k,v in REMAP.items():
+        s = s.replace(k, v)
+    return int(s, 2)
 
 def get_seat_ids(lines):
     return [get_seat_id(s) for s in lines]
-
 
 def main():
     with open('input.txt', 'r') as f:
