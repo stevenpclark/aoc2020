32cf28dd648ebd9d90dd7764caaf53ddceef25e2
Steven Clark <steven@macbook-pro.lan>
2020-12-14 13:22:37 -0500

    refactor cleanup

--- 14/14.py
+++ 14/14.py
@@ -8,7 +8,7 @@ def apply_mask(v, mask):
     return v
 
 
-def part1(cmds):
+def part1(cmds, use_direct_mode=True):
     arr = dict()
     mask = None
 
@@ -17,30 +17,20 @@ def part1(cmds):
             mask = v
         else:
             addr = int(c[4:-1])
-            arr[addr] = apply_mask(int(v), mask)
-    print(sum(arr.values()))
-
-
-def part2(cmds):
-    arr = dict()
-    mask = None
-    
-    for c, v in cmds:
-        if c == 'mask':
-            mask = v
-        else:
-            v = int(v)
-            addr_list = list(bin(int(c[4:-1]))[2:].zfill(36))
-            for i, m in enumerate(mask):
-                if m == '0':
-                    continue
-                addr_list[i] = m
-            x_inds = [i for i, c in enumerate(addr_list) if c=='X']
-            num_x = len(x_inds)
-            for bits in itertools.product(['0','1'], repeat=num_x):
-                for x_ind, x_val in zip(x_inds, bits):
-                    addr_list[x_ind] = x_val
-                arr[int(''.join(addr_list), 2)] = v
+            if use_direct_mode:
+                arr[addr] = apply_mask(int(v), mask)
+            else:
+                addr_list = list(bin(addr)[2:].zfill(36))
+                for i, m in enumerate(mask):
+                    if m == '0':
+                        continue
+                    addr_list[i] = m
+                x_inds = [i for i, c in enumerate(addr_list) if c=='X']
+                num_x = len(x_inds)
+                for bits in itertools.product(['0','1'], repeat=num_x):
+                    for x_ind, x_val in zip(x_inds, bits):
+                        addr_list[x_ind] = x_val
+                    arr[int(''.join(addr_list), 2)] = int(v)
     print(sum(arr.values()))
 
 
@@ -48,7 +38,7 @@ def main():
     with open('input.txt', 'r') as f:
         cmds = [li.rstrip().split(' = ') for li in f.readlines()]
     part1(cmds)
-    part2(cmds)
+    part1(cmds, use_direct_mode=False)
 
 
 if __name__ == '__main__':
